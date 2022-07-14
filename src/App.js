369579b50a045c61ff32198b1d84a6c89e71d771
arraayyy <20102056@usc.edu.ph>
2022-07-14 13:46:00 +0800

    problem fixed, styling added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+
+import Header from './components/Header';
+import NotesPage from './pages/NotesPage'
+import NotePage from './pages/NotePage'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container dark">
+        <div className="app">
+          <Header/>
+          <Route path="/" component={NotesPage} exact />
+          <Route path="/note/:id" component={NotePage} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/NotesPage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import notes from '../assets/data'
+import ListItem from '../components/ListItem'
+
+const NotesPage = () => {
+  return (
+    <div className='notes'>
+        <div className='notes-header'>
+            <h2 className='notes-title'>&#9782;Notes</h2>
+            <p className='notes-count'>{notes.length}</p>
+        </div>
+
+        <div className='notes-list'>
+            {notes.map((note, index) => (
+                <ListItem key={index} note={note}/>
+            ))}
+        </div>
+ 
+    </div>
+  )
+}
+
+export default NotesPage
